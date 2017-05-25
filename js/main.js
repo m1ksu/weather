ab024cc956516d7affdfc0854e2171ed587c7134
m1ksu <mikael.stenman112@gmail.com>
2017-05-25 19:28:40 +0300

    Some more initialization, now console.logs current coordinates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function () {
 
+    var geoOptions = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumWait: 2000,     // max wait time for desired accuracy
+        maximumAge: 0,          // disable cache
+        desiredAccuracy: 30,    // meters
+        fallbackToIP: true     // fallback to IP if Geolocation fails or rejected
+    }
+    
+    geolocator.locate(geoOptions, function (err, location) {
+        location = ([location.coords.latitude, location.coords.longitude]);
+        showLoc(location);
+    });
+
+    function showLoc(location) {
+        console.log(location);
+    }
+
+});
