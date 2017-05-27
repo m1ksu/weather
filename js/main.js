07350e90132180bbd80d8155e96e6dd274a671ed
m1ksu <mikael.stenman112@gmail.com>
2017-05-27 14:33:08 +0300

    Finished site :)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
 
-    var geoOptions = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumWait: 2000,     // max wait time for desired accuracy
-        maximumAge: 0,          // disable cache
-        desiredAccuracy: 30,    // meters
-        fallbackToIP: true     // fallback to IP if Geolocation fails or rejected
+    if (Modernizr.geolocation) {
+        navigator.geolocation.getCurrentPosition(posSuccess, posFail);
+    } else {
+        alert("Geolocation is not available.");
     }
-    
-    geolocator.locate(geoOptions, function (err, location) {
-        location = ([location.coords.latitude, location.coords.longitude]);
-        showLoc(location);
-    });
 
-    function showLoc(location) {
-        console.log(location);
+    function posSuccess (pos) {
+
+        var latitude = pos.coords.latitude;
+        var longitude = pos.coords.longitude;
+
+        http.get({
+            url: `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=795fbc06a67ded958fb79568ea4c7eae&units=metric`,
+            onload: function () {
+                setText(JSON.parse(this.responseText));
+            }
+        })
+    }
+
+    function posFail (e) {
+        console.log(e);
+    }
+
+    function setText (weather) {
+        console.log(weather);
+
+        $(".location-city").text(weather.name);
+        $(".temperature").text(weather.main.temp + "°C");
+        $(".condition-text").text(weather.weather[0].main);
+        $(".condition-icon").attr("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
     }
 
 });
